7b5043d9e89e188e4c72824748d6ea0fb43ae171
Holzlsauer <renato.holzlsauer@gmail.com>
2020-08-12 17:10:16 -0300

    Add style to Table

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const TableWrapper = styled.table`
+  border-collapse: collapse;
+`;
+
+TableWrapper.TD = styled.td`
+  border: 1px solid #FFFFFF;
+  padding: 7px;
+`;
+
+TableWrapper.TH = styled.th`
+  border: 1px solid #FFFFFF;
+  padding: 10px;
+`;
 
 const Header = ({ keys }) => (
   <thead>
     <tr>
-      { keys.map((key) => <th key={key}>{key}</th>) }
+      { keys.map((key) => <TableWrapper.TH key={key}>{key}</TableWrapper.TH>) }
     </tr>
   </thead>
 );
 
 const Row = ({ row, keys }) => (
   <tr key={row.id}>
-    { keys.map((index) => <td key={`${row.id}.${index}`}>{row[index]}</td>) }
+    { keys.map((index) => <TableWrapper.TD key={`${row.id}.${index}`}>{row[index]}</TableWrapper.TD>) }
   </tr>
 );
 
@@ -20,12 +35,12 @@ function Table({ data, head }) {
   const headValues = Object.values(head);
 
   return (
-    <table>
+    <TableWrapper>
       <Header keys={headValues} />
       <tbody>
         { data.map((row) => <Row keys={headKeys} row={row} />) }
       </tbody>
-    </table>
+    </TableWrapper>
   );
 }
 
